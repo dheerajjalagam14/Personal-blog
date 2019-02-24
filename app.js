f2bc02d420c04ab02c1db0515bfeb6963cfdc35f
Dheeraj <Dheeraj@Users-MacBook-Pro.local>
2019-02-23 19:09:57 -0600

    added gitignore and procfile

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.get("/posts/:postId", function(req, res){
     });
   });;
 
-app.listen(3000, function() {
+app.listen(env.process.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
