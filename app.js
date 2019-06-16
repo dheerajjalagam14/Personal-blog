f544be5fd23ba0f0003d71ffc8f856711491b563
Dheeraj <Dheeraj@users-mbp.attlocal.net>
2019-06-16 15:09:24 -0500

    edit ip access

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //jshint esversion:6
 
-const express = require("express");
+const express = require("express")
+const ipfilter = require("express-ipfilter").IpFilter
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const ips = ["192.168.1.91"];
 mongoose.connect('mongodb+srv://admin-jalagam:jalagam@cluster-blog-tg8ls.mongodb.net/blogDB', {useNewUrlParser: true});
 
 const postSchema = new mongoose.Schema({
@@ -23,7 +25,7 @@ const Post = mongoose.model("Post",postSchema);
 
 const homeStartingContent = "How's it going by the way ?  Since you already know my name and enough about me, I wouldnt waste your time here. I mostly write here anything that I'd like to stand out. Take some time, Feel free to read anything which interests you.";
 const aboutContent = "I am Dheeraj Kumar Jalagam, an aspiring Web developer from Dallas, Texas. I am a Proud Alumni of University Of Southern Mississippi with a Masters title in Computer Science. I ❤️ to code and my code needs ☕️  to compile.  "
-const contactContent = "for everything else, please 📧  and I will get back to you asap. Thank You";
+const contactContent = "Please 📧  and I will get back to you asap. Thank You";
 
 const app = express();
 
@@ -51,11 +53,11 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", ipfilter(ips, { mode: 'allow' }), function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", ipfilter(ips, { mode: 'allow' }), function(req, res){
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
@@ -66,7 +68,6 @@ app.post("/compose", function(req, res){
       res.redirect("/");
     }
   });
-
 });
 
 app.get("/posts/:postId", function(req, res){
@@ -78,7 +79,7 @@ app.get("/posts/:postId", function(req, res){
         content: post.content
       });
     });
-  });;
+});
 
 app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
